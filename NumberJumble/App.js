2095d92d3bfd2f10480578b1e48ad934dab1a05e
SaidNivek <kevin.vi.dias@gmail.com>
2023-03-25 22:50:25 -0400

    Begin navigation

--- NumberJumble/App.js
+++ NumberJumble/App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigate/native'
+import { createNativeStackNavigator } from '@react-navigate/native-stack'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer >
+      <ScreenStack.Navigator>
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <StatusBar style="auto" />
+        </View>
+      </ScreenStack.Navigator>
+    </NavigationContainer>
   );
 }
 
